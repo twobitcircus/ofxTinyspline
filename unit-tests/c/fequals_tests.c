5dd5b53b011d7f73e84dc545b637c07b854cdcda
Marcel <github@retux.de>
2016-01-26 11:11:40 +0100

    added more tests

--- unit-tests/c/fequals_tests.c
+++ unit-tests/c/fequals_tests.c
@@ -23,6 +23,7 @@ void fequals_test_equals(CuTest* tc)
 
 void fequals_test_unequals(CuTest* tc)
 {
+    /* test zero with positive */
     CuAssertTrue(tc, ts_fequals(0.f, 1.f) == 0);
     CuAssertTrue(tc, ts_fequals(1.f, 0.f) == 0);
     CuAssertTrue(tc, ts_fequals(0.f, 2.f) == 0);
@@ -32,6 +33,7 @@ void fequals_test_unequals(CuTest* tc)
     CuAssertTrue(tc, ts_fequals(0.f, 1000000.f) == 0);
     CuAssertTrue(tc, ts_fequals(1000000.f, 0.f) == 0);
 
+    /* test both positive*/
     CuAssertTrue(tc, ts_fequals(1.f, 2.f) == 0);
     CuAssertTrue(tc, ts_fequals(2.f, 1.f) == 0);
     CuAssertTrue(tc, ts_fequals(200.f, 300.f) == 0);
@@ -39,6 +41,7 @@ void fequals_test_unequals(CuTest* tc)
     CuAssertTrue(tc, ts_fequals(20000.f, 30000.f) == 0);
     CuAssertTrue(tc, ts_fequals(30000.f, 20000.f) == 0);
 
+    /* test zero with negative */
     CuAssertTrue(tc, ts_fequals(0.f, -1.f) == 0);
     CuAssertTrue(tc, ts_fequals(-1.f, 0.f) == 0);
     CuAssertTrue(tc, ts_fequals(0.f, -2.f) == 0);
@@ -48,6 +51,7 @@ void fequals_test_unequals(CuTest* tc)
     CuAssertTrue(tc, ts_fequals(0.f, -1000000.f) == 0);
     CuAssertTrue(tc, ts_fequals(-1000000.f, 0.f) == 0);
 
+    /* test both negative */
     CuAssertTrue(tc, ts_fequals(-1.f, -2.f) == 0);
     CuAssertTrue(tc, ts_fequals(-2.f, -1.f) == 0);
     CuAssertTrue(tc, ts_fequals(-200.f, -300.f) == 0);
@@ -55,6 +59,7 @@ void fequals_test_unequals(CuTest* tc)
     CuAssertTrue(tc, ts_fequals(-20000.f, -30000.f) == 0);
     CuAssertTrue(tc, ts_fequals(-30000.f, -20000.f) == 0);
 
+    /* test one positive one negative */
     CuAssertTrue(tc, ts_fequals(1.f, -1.f) == 0);
     CuAssertTrue(tc, ts_fequals(-1.f, 1.f) == 0);
     CuAssertTrue(tc, ts_fequals(2.f, -2.f) == 0);
@@ -64,23 +69,52 @@ void fequals_test_unequals(CuTest* tc)
     CuAssertTrue(tc, ts_fequals(1000000.f, -1000000.f) == 0);
     CuAssertTrue(tc, ts_fequals(-1000000.f, 1000000.f) == 0);
 
+    /* test some random values without same same abs value */
     CuAssertTrue(tc, ts_fequals(200.f, -586.f) == 0);
     CuAssertTrue(tc, ts_fequals(-586.f, 200.f) == 0);
+    CuAssertTrue(tc, ts_fequals(-98744.f, 13818.f) == 0);
+    CuAssertTrue(tc, ts_fequals(-13818.f, 98744.f) == 0);
 }
 
 void fequals_test_small(CuTest* tc)
 {
-    /* This should be the minimum difference that should
-     * be supported by fequals */
+    /* tests around 0 */
     CuAssertTrue(tc, ts_fequals(0, 0.0001f) == 0);
-    CuAssertTrue(tc, ts_fequals(1, 1.0001f) == 0);
-    CuAssertTrue(tc, ts_fequals(2, 2.0001f) == 0);
-    CuAssertTrue(tc, ts_fequals(1000, 1000.0001f) == 0);
-    CuAssertTrue(tc, ts_fequals(-0, -0.0001f) == 0);
-    CuAssertTrue(tc, ts_fequals(-1, -1.0001f) == 0);
-    CuAssertTrue(tc, ts_fequals(-2, -2.0001f) == 0);
-    CuAssertTrue(tc, ts_fequals(-1000, -1000.0001f) == 0);
+    CuAssertTrue(tc, ts_fequals(0, -0.0001f) == 0);
+    CuAssertTrue(tc, ts_fequals(0, 0.00001f) == 1);
+    CuAssertTrue(tc, ts_fequals(0, -0.00001f) == 1);
 
+    /* tests between 0 and max */
+    CuAssertTrue(tc, ts_fequals(100, 100.0001f) == 0);
+    CuAssertTrue(tc, ts_fequals(100, 99.9999f) == 0);
+    CuAssertTrue(tc, ts_fequals(100, 100.00001f) == 1);
+    CuAssertTrue(tc, ts_fequals(100, 99.99999f) == 1);
+
+    CuAssertTrue(tc, ts_fequals(-100, -100.0001f) == 0);
+    CuAssertTrue(tc, ts_fequals(-100, -99.9999f) == 0);
+    CuAssertTrue(tc, ts_fequals(-100, -100.00001f) == 1);
+    CuAssertTrue(tc, ts_fequals(-100, -99.99999f) == 1);
+
+    CuAssertTrue(tc, ts_fequals(1000, 1000.0001f) == 0);
+    CuAssertTrue(tc, ts_fequals(1000, 999.9999f) == 0);
+    CuAssertTrue(tc, ts_fequals(1000, 1000.00001f) == 1);
+    CuAssertTrue(tc, ts_fequals(1000, 999.99999f) == 1);
+
+    CuAssertTrue(tc, ts_fequals(-1000, -1000.0001f) == 0);
+    CuAssertTrue(tc, ts_fequals(-1000, -999.9999f) == 0);
+    CuAssertTrue(tc, ts_fequals(-1000, -1000.00001f) == 1);
+    CuAssertTrue(tc, ts_fequals(-1000, -999.99999f) == 1);
+
+    /* tests around max */
+    CuAssertTrue(tc, ts_fequals(-2047, -2047.0001f) == 0);
+    CuAssertTrue(tc, ts_fequals(-2048, -2047.9999f) == 0);
+    CuAssertTrue(tc, ts_fequals(-2047, -2047.00001f) == 1);
+    CuAssertTrue(tc, ts_fequals(-2048, -2047.99999f) == 1);
+
+    CuAssertTrue(tc, ts_fequals(2047, 2047.0001f) == 0);
+    CuAssertTrue(tc, ts_fequals(2048, 2047.9999f) == 0);
+    CuAssertTrue(tc, ts_fequals(2047, 2047.00001f) == 1);
+    CuAssertTrue(tc, ts_fequals(2048, 2047.99999f) == 1);
 }
 
 CuSuite* get_fequals_suite()
